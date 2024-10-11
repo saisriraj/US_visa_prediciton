371257a064edb1d61e106e60f06a741118948ac5
sri raj <saisriraj.5@gmail.com>
2024-10-11 00:56:46 -0500

    modified temp

--- template.py
+++ template.py
@@ -1,1 +1,21 @@
-import pathlib
+
+import os
+from pathlib import Path
+
+project_name = 'us_visa'
+
+list_of _folders= [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_train.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/
+
+]
